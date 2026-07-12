85208f9905c358ce9c2faa656e1bd1eb45933b41
Jeret McCoy <jeretmccoy@gmail.com>
2026-07-11 20:37:53 -0400

    bundle card record checksum fix (1.0.87)

--- qt/aqt/_kelma_bundled.py
+++ qt/aqt/_kelma_bundled.py
@@ -14,7 +14,7 @@ from __future__ import annotations
 import os
 import shutil
 
-BUNDLED_VERSION = "1.0.86"
+BUNDLED_VERSION = "1.0.87"
 ADDON = "kelma"
 _MARKER = ".kelma_bundled_version"
 
--- qt/aqt/data/bundled_addons/kelma/kelma/kelma_sync_v2/anki_local.py
+++ qt/aqt/data/bundled_addons/kelma/kelma/kelma_sync_v2/anki_local.py
@@ -14,6 +14,7 @@ from typing import Any
 from anki.collection import Collection
 
 from .checksum_rs import (
+    card_checksum,
     card_checksums_batch,
     deck_checksum,
     note_checksum,
